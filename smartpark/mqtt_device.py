ce110e31955ef3ba0d23e317c1aab2097e3bc0d2
ca20110820 <ca20110820@gmail.com>
2023-10-30 20:07:50 +0800

    Implemented MqttDevice class

--- smartpark/mqtt_device.py
+++ smartpark/mqtt_device.py
@@ -2,25 +2,26 @@ import paho.mqtt.client as paho
 
 
 class MqttDevice:
-    def __init__(self, config):
-        self.name = config['name']
-        self.location = config['location']
+    def __init__(self, config: dict, keepalive: int = 65535, *args, **kwargs):
+        self.topic_root = config["topic-root"]
+        self.location = config["location"]
+        self.name = config["name"]
+        self.topic_qualifier = config["topic-qualifier"]
+        self.host = config["host"]
+        self.port = config["port"]
 
-        # Define topic components
-        self.topic_root = config['topic-root']
-        self.topic_qualifier = config['topic-qualifier']
-        self.topic = self._create_topic_string()
+        self.client: paho.Client = paho.Client(*args, **kwargs)
+        self.client.connect(self.host, self.port, keepalive=keepalive)
 
-        # Configure broker
-        self.broker = config['broker']
-        self.port = config['port']
+    @property
+    def topic_address(self) -> str:
+        # Topic Address/Directory: <topic-root>/<location>/<name>/<topic-qualifier>
+        # Note: This is the standard/default template for the topic address.
+        return f"{self.topic_root}/{self.location}/{self.name}/{self.topic_qualifier}"
 
-        self.client = paho.Client()
-        self.client.connect(self.broker,
-                            self.port,
-                            keepalive=65535
-                            )
+    def create_topic_address(self, custom_location: str, custom_name: str, custom_topic_qualifier: str):
+        # Note: Topic Root cannot be changed.
+        return f"{self.topic_root}/{custom_location}/{custom_name}/{custom_topic_qualifier}"
 
-    def _create_topic_string(self):
-        return (f"{self.topic_root}/{self.location}/" +
-                f"{self.name}/{self.topic_qualifier}")
+    def create_topic_qualifier(self, custom_topic_qualifier: str):
+        return f"{self.topic_root}/{self.location}/{self.name}/{custom_topic_qualifier}"
