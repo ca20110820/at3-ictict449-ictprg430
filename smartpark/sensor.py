d56cd44d3c84d9738433b481bbcd9795a93278e7
ca20110820 <ca20110820@gmail.com>
2023-11-01 14:01:41 +0800

    Updated sensor module

--- smartpark/sensor.py
+++ smartpark/sensor.py
@@ -114,7 +114,24 @@ class BaySensor(Sensor):
             print(f"There are no car parked in Bay {self.name}!")
 
 
-class CLISensor(BaySensor):
+class CLICarParkSensor(CarParkSensor):
+    def start_sensing(self, *args, **kwargs):
+        """Implement with Event loop"""
+        pass
+
+    def on_car_entered(self):
+
+        topic = self.create_topic_qualifier("na")
+        message = f"Enter,{self.temperature},{self.get_time_now_as_str()}"
+        self.client.publish(topic, message)
+
+    def on_car_exited(self):
+        topic = self.create_topic_qualifier("na")
+        message = f"Exit,{self.temperature},{self.get_time_now_as_str()}"
+        self.client.publish(topic, message)
+
+
+class CLIBaySensorSensor(BaySensor):
     def __init__(self, config, *args, **kwargs):
         super().__init__(config, *args, **kwargs)
 
@@ -145,94 +162,6 @@ class CLISensor(BaySensor):
                 continue
 
 
-class GUICarDetector(BaySensor):
-    def __init__(self, config, *args, **kwargs):
-        super().__init__(config, *args, **kwargs)
-        self.root = tk.Tk()
-        self.root.title(f"Car Detector ULTRA | Bay: {self.name}")
-
-        self.btn_incoming_car = tk.Button(
-            self.root, text='🚘 Incoming Car', font=('Arial', 50), cursor='right_side', command=self.on_car_parked)
-        self.btn_incoming_car.pack(padx=10, pady=5)
-        self.btn_outgoing_car = tk.Button(
-            self.root, text='Outgoing Car 🚘', font=('Arial', 50), cursor='bottom_left_corner',
-            command=self.on_car_unparked)
-        self.btn_outgoing_car.pack(padx=10, pady=5)
-
-    @property
-    def temperature(self):
-        # TODO: Import a callable from temperature.py to get the temperature
-        return random.randint(10, 30)
-
-    def start_sensing(self):
-        self.root.mainloop()
-
-    def on_car_parked(self):
-        # Topic: <topic-root>/<location>/<bay_id_or_name>/entry
-        # Message: "Entry,<license_plate>,<car_model>,<temperature>"  Need to be parsed in Subscribers
-        if not self.IS_OCCUPIED:
-            self.client.publish(self.create_topic_qualifier("entry"),
-                                f"Entry,{datetime.datetime.now()},<license_plate>,<car_model>,{self.temperature}")
-            self.IS_OCCUPIED = True
-        else:
-            print(f"There's already a car parked in {self.name}!")
-
-    def on_car_unparked(self):
-        # Topic: <topic-root>/<location>/<bay_id_or_name>/exit
-        # Message: "Entry,<temperature>"
-
-        if self.IS_OCCUPIED:
-            self.client.publish(self.create_topic_qualifier("exit"),
-                                f"Exit,{self.temperature}")
-            self.IS_OCCUPIED = False
-        else:
-            print(f"Cannot exit when there is no car in {self.name}!")
-
-
-class SimulatedSensor(Sensor):
-    def __init__(self, config, *args, **kwargs):
-        super().__init__(config, *args, **kwargs)
-
-    @property
-    def temperature(self):
-        """Implement Getter for Temperature, this can be random or pulled from API"""
-        return random.randint(20, 30)
-
-    def start_sensing(self, *args, **kwargs):
-        while True:
-            time_interval = 1  #
-            time.sleep(time_interval)
-            rnd = random.choice(["Entry", "Exit"])
-            if rnd == "Entry":
-                self.on_car_entry()
-            elif rnd == "Exit":
-                self.on_car_exit()
-            else:
-                continue
-
-    def on_car_entry(self):
-        # Topic: <topic-root>/<location>/<bay_id_or_name>/entry
-        # Message: "Entry,<license_plate>,<car_model>,<temperature>"  Need to be parsed in Subscribers
-        if not self.IS_OCCUPIED:
-            print("Car Entered")
-            self.client.publish(self.create_topic_qualifier("entry"),
-                                f"Entry,{datetime.datetime.now()},<license_plate>,<car_model>,{self.temperature}")
-            self.IS_OCCUPIED = True
-        else:
-            print(f"There's already a car parked in {self.name}!")
-
-    def on_car_exit(self):
-        # Topic: <topic-root>/<location>/<bay_id_or_name>/exit
-        # Message: "Entry,<temperature>"
-        if self.IS_OCCUPIED:
-            print("Car Exited")
-            self.client.publish(self.create_topic_qualifier("exit"),
-                                f"Exit,{self.temperature}")
-            self.IS_OCCUPIED = False
-        else:
-            print(f"Cannot exit when there is no car in {self.name}!")
-
-
 if __name__ == '__main__':
     sensor_config = {"name": "bay_1",
                      "location": "L306",
@@ -243,6 +172,6 @@ if __name__ == '__main__':
                      }
     # Topic: "Moondaloop Park/L306/bay_1/na"
 
-    CLISensor(sensor_config).start_sensing()
+    # CLISensor(sensor_config).start_sensing()
     # GUICarDetector(sensor_config).start_sensing()
     # SimulatedSensor(sensor_config).start_sensing()
